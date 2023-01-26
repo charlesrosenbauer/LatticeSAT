e3e8d233fd75d458677739f8952e4082a3f5513a
Charles <charles.rosenbauer@gmail.com>
2023-01-25 18:45:57 -0600

    Circuit structs, etc.

--- src/tseytin.h
+++ src/tseytin.h
@@ -4,9 +4,34 @@
 
 #include "stdint.h"
 
+#include "sat.h"
+
+
+typedef enum{
+	G_EQ	= 0,
+	G_NOT	= 1,
+	G_AND	= 2,
+	G_NAND	= 3,
+	G_OR	= 4,
+	G_NOR	= 5,
+	G_XOR	= 6,
+	G_XNOR	= 7
+}GateKind;
+
+typedef struct{
+	uint32_t a, b, c;
+	GateKind k;
+}Gate;
+
+typedef struct{
+	Gate*	gates;
+	int		fill, size, wirect;
+}Circuit;
 
 
 
+Circuit		allocCircuit(int);
+Instance 	circuitToSAT(Circuit);
 
 
 
