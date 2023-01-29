02d58f3fe6ae1e92b4a28bf362fb8677ed9e35b7
Charles <charles.rosenbauer@gmail.com>
2023-01-28 22:37:59 -0600

    Notes on solver stuff

---
+++ src/solver.c
@@ -0,0 +1,22 @@
+#include "stdint.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+#include "sat.h"
+
+
+/*
+	TODO:
+	* start with just a simple, ordinary DPLL solver
+	* debug on 8 bit case solved by hand
+	* build local neighborhoods to make predictions
+	* add predictions, test performance difference and increase difficulty
+	* wave function collapse as optimization
+	* CDCL-like approach of adding new neighborhoods on big backtracks
+*/
+int solver(Instance* sat){
+	
+	
+	return 0;
+}
+
