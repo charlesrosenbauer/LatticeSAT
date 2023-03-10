9b3a3ad4d0ec8a7de92038c27f7ebce5a22e1e19
Charles <charles.rosenbauer@gmail.com>
2023-03-09 19:55:44 -0600

    DFS solver fixes, a pretty histogram plotter that outputs the exact dynamics I expected to see

--- src/main.c
+++ src/main.c
@@ -5,9 +5,27 @@
 #include "util.h"
 
 
+void printHistogram(int* xs, int ct){
+	float max = xs[0];
+	for(int i = 1; i < ct; i++) max = (max < xs[i])? xs[i] : max;
+	
+	for(int i = 0; i < ct; i++){
+		float n = (((float)xs[i]) / max) * 64.0;
+		printf("%04i| ", i);
+		for(int j = 0; j < 64; j++)
+			if(j < n) printf("#");
+		printf("\n");
+	}
+}
+
+
 int main(){
-	Instance sat = randomSAT(17, 16);
+	Instance sat = randomSAT(192, 64);
 	printSAT(sat);
+	
+	
+	int     table[128];
+	for(int i = 0; i < 128; i++) table[i] = 0;
 	
 	int      back = 0;
 	int        ix = 1;
@@ -15,7 +33,7 @@ int main(){
 	uint64_t pred[4];
 	for(int i = 0; i < 4; i++) flip[i] = pred[i] = 0;
 	
-	while((ix > 0) && (ix <= 16)){
+	while((ix > 0) && (ix <= sat.vct)){
 		uint64_t m = 1l << (ix%64);
 		uint64_t f =  flip[ix/64] & m;
 		uint64_t p = (pred[ix/64] & m) ^ f;
@@ -25,18 +43,21 @@ int main(){
 			continue;
 		}else if(back){
 			f ^= m;
+			p ^= m;
 			flip[ix/64] ^= m;
 			back = 0;
 		}else{
 			back = 0;
 		}
 		
+		table[ix]++;
+		
 		sat.bits[ix/64] &= ~m;
 		sat.bits[ix/64] |=  p;
 		
 		int     bk = 0;
 		int   pass = 1;
-		printf("GUESS %i : %c\n", ix, p? '1' : '0');
+		//printf("GUESS %i : %c\n", ix, p? '1' : '0');
 		for(int i = 0; i < sat.cct; i++){
 			Clause cs = sat.cs[i];
 			int a = cs.a < 0? -cs.a : cs.a;
@@ -48,25 +69,26 @@ int main(){
 					bk = a;
 					if((b != ix) && (b > bk) || (bk == ix)) bk = b;
 					if((c != ix) && (c > bk)              ) bk = c;
-					printf("  BT %i %i %i [%i]\n", cs.a, cs.b, cs.c, bk);
+					//printf("  BT %i %i %i [%i]\n", cs.a, cs.b, cs.c, bk);
 				}
 			}
 		}
 		
 		if(pass){
-			printf("PASS\n");
+			//printf("PASS\n");
 			flip[(ix+1)/64] &= ~(1l << ((ix+1)%64));
 			ix++;
 		}else if(f){
-			printf("BACK\n");
+			//printf("BACK\n");
 			flip[ ix   /64] &= ~m;
 			back = 1;
 			ix = bk;
 		}else{
-			printf("FAIL\n");
+			//printf("FAIL\n");
 			flip[ix/64] ^=  m;
 		}
 	}
 	
 	printf("%i\n", checkAssignment(sat));
+	printHistogram(table, sat.vct+1);
 }
