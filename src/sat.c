f5e3d3febd3d600564adc29060b9c4a840cb35d7
Charles <charles.rosenbauer@gmail.com>
2023-03-09 22:43:02 -0600

    Timing benchmark

--- src/sat.c
+++ src/sat.c
@@ -7,6 +7,20 @@
 #include "util.h"
 
 
+void printHistogram(int* xs, int ct){
+	float max = xs[0];
+	for(int i = 1; i < ct; i++) max = (max < xs[i])? xs[i] : max;
+	
+	for(int i = 0; i < ct; i++){
+		float n = (((float)xs[i]) / max) * 64.0;
+		printf("%04i| ", i);
+		for(int j = 0; j < 64; j++)
+			if(j < n) printf("#");
+		printf("\n");
+	}
+}
+
+
 Instance randomSAT(int cct, int vct){
 	assert(vct < cct);
 	assert(vct >   1);
@@ -126,5 +140,111 @@ void annealing(Instance sat){
 	}
 	
 	printf("%i / %i\n", init, checkAssignment(sat));
+}
+
+
+
+int solver(Instance sat, int* cts){
+	if(cts != NULL)
+		for(int i = 0; i <= sat.vct; i++) cts[i] = 0;
+
+	int size = (sat.vct / 64) + 1;
+
+	int*     table = alloca(sizeof(uint64_t) * size);
+	for(int i = 0; i < size; i++) table[i] = 0;
+	
+	int       back = 0;
+	int         ix = 1;
+	uint64_t* flip = alloca(sizeof(uint64_t) * size);
+	uint64_t* pred = alloca(sizeof(uint64_t) * size);
+	for(int i = 0; i < size; i++) flip[i] = pred[i] = 0;
+	
+	while((ix > 0) && (ix <= sat.vct)){
+		uint64_t m = 1l << (ix%64);
+		uint64_t f =  flip[ix/64] & m;
+		uint64_t p = (pred[ix/64] & m) ^ f;
+		
+		if(f && back){
+			ix--;
+			continue;
+		}else if(back){
+			f ^= m;
+			p ^= m;
+			flip[ix/64] ^= m;
+			back = 0;
+		}else{
+			back = 0;
+		}
+		
+		if(cts != NULL) cts[ix]++;
+		
+		sat.bits[ix/64] &= ~m;
+		sat.bits[ix/64] |=  p;
+		
+		int     bk = 0;
+		int   pass = 1;
+		//printf("GUESS %i : %c\n", ix, p? '1' : '0');
+		for(int i = 0; i < sat.cct; i++){
+			Clause cs = sat.cs[i];
+			int a = cs.a < 0? -cs.a : cs.a;
+			int b = cs.b < 0? -cs.b : cs.b;
+			int c = cs.c < 0? -cs.c : cs.c;
+			if((a <= ix) && (b <= ix) && (c <= ix) && pass){
+				pass = checkClause(sat, i);
+				if(!pass){
+					bk = a;
+					if((b != ix) && (b > bk) || (bk == ix)) bk = b;
+					if((c != ix) && (c > bk)              ) bk = c;
+					//printf("  BT %i %i %i [%i]\n", cs.a, cs.b, cs.c, bk);
+				}
+			}
+		}
+		
+		if(pass){
+			//printf("PASS\n");
+			flip[(ix+1)/64] &= ~(1l << ((ix+1)%64));
+			ix++;
+		}else if(f){
+			//printf("BACK\n");
+			flip[ ix   /64] &= ~m;
+			back = 1;
+			ix = bk;
+		}else{
+			//printf("FAIL\n");
+			flip[ix/64] ^=  m;
+		}
+	}
+	
+	return ix > 0;
+}
+
+
+
+void solverBenchmark(int head, int tail, int skip, int size){
+	int val = 0;
+	int max = 0;
+	for(int i = head; i < tail; i += skip){
+		Instance sat = randomSAT(i, size);
+		//printSAT(sat);
+	
+		int* table = alloca(sizeof(int) * (size + 4));
+		solver(sat, table);
+	
+		printf("%i\n", checkAssignment(sat));
+		printHistogram(table, sat.vct+1);
+		
+		int t = 0;
+		for(int j = 1; j <= sat.vct; j++) t += table[j];
+		
+		if(val < t){
+			val = t;
+			max = i;
+		}
+		printf(">>>> %i %i\n", i, t);
+		
+		free(sat.cs  );
+		free(sat.bits);
+	}
+	printf("MAX = %i : %i\n", val, max);
 }
 
--- src/sat.h
+++ src/sat.h
@@ -20,9 +20,11 @@ Instance	randomSAT		(int, int);
 int			checkAssignment	(Instance);
 void		printSAT		(Instance);
 int			checkClause		(Instance, int);
+int			solver			(Instance, int*);
+void		solverBenchmark	(int, int, int, int);
 
 
-
+void		printHistogram	(int*, int);
 
 
 
