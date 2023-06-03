0e3ba880517a6dd2bfccfe9fc5842a2ceb97b6c9
Charles <charles.rosenbauer@gmail.com>
2023-06-03 02:21:07 -0500

    Rough visualization, eventually I want clauses/neighborhoods to be visualized somehow though

--- src/main.c
+++ src/main.c
@@ -4,13 +4,50 @@
 #include "sat.h"
 #include "util.h"
 
+#include "SDL/SDL.h"
+
 
 
 int main(){
-	Instance       sat = randomSAT(61, 16);
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Surface* screen = SDL_SetVideoMode(512, 512, 32, 0);
+
+	Instance       sat = randomSAT(976, 256);
 	DecorInstance dsat = sortInstance(sat);
 	printDecorSAT(dsat);
 	
 	PathSolver solv = initPathSolver(&dsat);
-	printf("SAT=%i/%i\n", pathSolve(&solv, 100), dsat.cct);
+	
+	
+	int n = sat.vct;
+	{
+		while((n * n) > sat.vct){
+			n /= 2;
+		}
+	}
+	printf("RT = %i\n", n);
+	int scale = 512 / n;
+	
+	
+	int cont = 1;
+	while(cont){
+		SDL_Event e;
+		while(SDL_PollEvent(&e)) if(e.type == SDL_QUIT) cont = 0;
+		
+		uint32_t* pix = screen->pixels;
+		int ix = 0;
+		for(int i = 0; i < 512; i++){
+			for(int j = 0; j < 512; j++){
+				int k = ((i/scale) * n) + (j/scale);
+				pix[ix] = (k < sat.vct)? ((sat.bits[k/64] & (1l << (k%64)))? 0xff0000 : 0xff) : 0;
+				ix++;
+			}
+		}
+		
+		printf("SAT=%i/%i\n", pathSolve(&solv, 100), dsat.cct);
+	
+		SDL_Flip(screen);
+		SDL_Delay(30);
+	}
+	SDL_Quit();
 }
